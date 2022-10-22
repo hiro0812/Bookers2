72d4004a6e415a6d28e5c8ec119f64423add882f
EC2 Default User <ec2-user@ip-172-31-8-123.ap-northeast-1.compute.internal>
2022-10-22 01:41:51 +0000

    [FINISH]1021

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @book = Book.new #空のカラムを作成
     @users = User.all #データ（レコード）を全件取得
-
   end
 
   def show
-    @user = User.find(params[:id])
-    @books = @user.books #アソシエーションを持っているモデル同士の記述
-    @book = Book.new
+    @user = User.find(params[:id])  # データ（レコード）を1件取得
+    # @books = @user.books #アソシエーションを持っているモデル同士の記述
+    @book_new = Book.new #空のカラムを作成
+    @book = Book.find(params[:id]) # データ（レコード）を1件取得
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = User.find(params[:id])  # データ（レコード）を1件取得
   end
 
   def update
